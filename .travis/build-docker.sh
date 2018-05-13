78ea450f14bc427eaa8d14ced6644882cd91525e
Craig A <craig.allwardt@pnnl.gov>
2018-05-12 21:46:17 -0700

    update .travis.yml to  not use the bootstrap.py from the other repo

--- .travis/build-docker.sh
+++ .travis/build-docker.sh
@@ -13,7 +13,7 @@ while getopts bp option ; do
   case $option in
     b) # Pass gridappsd tag to docker-compose
       # Docker file on travis relative from root.
-      docker build -t $IMAGE .
+      docker build --no-cache -t $IMAGE .
       ;;
     p) # Pass gridappsd tag to docker-compose
       docker push $IMAGE
