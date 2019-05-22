9f8d2e85a8ecd26a832c44631a942f0df5ae380f
Ben LaRoque <benjamin.laroque@gmail.com>
2019-05-22 14:34:13 -0700

    bootstart now exits with expected error code

--- core/bootstart.sh
+++ core/bootstart.sh
@@ -10,10 +10,23 @@ fi
 echo "Right before setup-platform.py is called I am calling printenv"
 printenv
 python /startup/setup-platform.py
-PID=$?
-echo "PID WAS $PID"
-if [[ "$PID" == "0" ]]; then
-    volttron -vv
+setup_return=$?
+if [[ $setup_return ]]; then
+    echo "error running setup-platform.py"
+    exit $setup_return
 fi
 
+# TODO
+# does setup-playtform.py return its PID? or do you not mean PID?
+# should `volttron -vv` be run if setup-platform fails?
 
+#PID=$?
+#echo "PID WAS $PID"
+#if [[ "$setup_return" == "0" ]]; then
+    volttron -vv
+    volttron_retcode=$?
+#fi
+if [[ $volttron_retcode ]]; then
+  echo "volttron error"
+  exit $volttron_retcode
+fi
