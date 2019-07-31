9ddf1b2fe9db345cc4bf306ee09520faf3acd05b
Ben LaRoque <benjamin.laroque@gmail.com>
2019-07-31 15:17:45 -0700

    don't break docker please

--- .travis/setup_emulation.sh
+++ .travis/setup_emulation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-echo '{"experimental":true}' | sudo tee -a /etc/docker/daemon.json
-sudo service docker restart
+## these are redundant with main.sh
+#echo '{"experimental":true}' | sudo tee -a /etc/docker/daemon.json
+#sudo service docker restart
 
 sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
