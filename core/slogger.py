a5f3b307f486f9c9cba7e27db0bcf04aee5a0eb8
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-10-29 21:15:15 -0700

    Cleanup code using black

--- core/slogger.py
+++ core/slogger.py
@@ -9,9 +9,11 @@ from datetime import datetime
 from pathlib import Path
 from logging.handlers import RotatingFileHandler
 
-FORMATTER = logging.Formatter(fmt="%(asctime)s %(levelname)s %(name)s %(funcName)s:%(lineno)d — %(message)s",
-                              datefmt='%Y-%m-%d %H:%M:%S')
-MAX_LOG_FILE_SIZE = 2 * math.pow(10, 6)       # 2 MB
+FORMATTER = logging.Formatter(
+    fmt="%(asctime)s %(levelname)s %(name)s %(funcName)s:%(lineno)d — %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+MAX_LOG_FILE_SIZE = 2 * math.pow(10, 6)  # 2 MB
 
 
 def get_console_handler():
@@ -21,9 +23,9 @@ def get_console_handler():
 
 
 def get_file_handler(logfile):
-    file_handler = RotatingFileHandler(filename=logfile,
-                                       maxBytes=MAX_LOG_FILE_SIZE,
-                                       backupCount=10)
+    file_handler = RotatingFileHandler(
+        filename=logfile, maxBytes=MAX_LOG_FILE_SIZE, backupCount=10
+    )
     file_handler.setFormatter(FORMATTER)
     return file_handler
 
@@ -38,9 +40,9 @@ def make_logger(logger_name, logfile):
 
 
 def get_logger(logger_name, log_prefix):
-    curr_date_time = datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
-    log_dir = 'logs'
+    curr_date_time = datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
+    log_dir = "logs"
     Path(log_dir).mkdir(parents=True, exist_ok=True)
-    logfile_name = log_prefix + '-' + curr_date_time + '.log'
+    logfile_name = log_prefix + "-" + curr_date_time + ".log"
     logfile_path = os.path.join(log_dir, logfile_name)
     return make_logger(logger_name=logger_name, logfile=logfile_path)
